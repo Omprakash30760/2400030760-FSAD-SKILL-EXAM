bddfad30103d383f54ba96fecdf7252c53ed6d89
Omprakash30760 <omprakashjena1606@gmail.com>
2026-03-10 08:45:58 +0530

    commit changes done

--- fsadskill/src/main/java/com/klef/fsad/exam/ClientDemo.java
+++ fsadskill/src/main/java/com/klef/fsad/exam/ClientDemo.java
@@ -17,23 +17,21 @@ public class ClientDemo
 
         Transaction tx = session.beginTransaction();
 
-        // Insert records
-        Movie m1 = new Movie(1,"RRR","2022","Released");
-        Movie m2 = new Movie(2,"Pushpa","2021","Released");
+        Movie m1 = new Movie(1,"Sholay","2003","Released");
+        Movie m2 = new Movie(2,"Bhramastra","2024","Released");
 
         session.save(m1);
         session.save(m2);
 
         tx.commit();
 
-        // HQL Update using positional parameters
         Transaction tx2 = session.beginTransaction();
 
         Query q = session.createQuery(
         "update Movie set name=?1, status=?2 where id=?3");
 
-        q.setParameter(1,"Salaar");
-        q.setParameter(2,"Upcoming");
+        q.setParameter(1,"Dhurandhar 2");
+        q.setParameter(2,"Upcoming next month");
         q.setParameter(3,1);
 
         int n = q.executeUpdate();
